506071cb03a055cf27b1a11c9793fbc9a40665a8
Tom Hrabchak <tom.hrabchak@qt.io>
2022-07-21 12:13:04 -0400

    Add basic structure

--- main.py
+++ main.py
@@ -2,27 +2,32 @@ import os
 import pandas as pd
 
 def main():
-	data_folder = os.path.abspath("data")
 	data_file = "fped_1718_trh.csv"
+
+	data_folder = os.path.abspath("data")
+	data_file_path = os.path.join(data_folder, data_file)
 
 	# 1. Load csv
 	printStep("Step 1: Loading CSV")
-	load(os.path.join(data_folder, data_file))
+	df = load(data_file_path)
 
 	# 2. Identify all base ingredients
 	printStep("Step 2: Find all base ingredients")
+	base_ingredients = findBaseIngredients(df)
 
 	# 3. Identify all foods
 	printStep("Step 3: Find all foods")
+	foods = findFoods(df)
 
 	# 4. Create a new table
 	printStep("Step 4: Create a new table of foods")
-		# Iterate through each food and create rows for each ingredient in the food
+	new_table = createNewTable(df, foods, base_ingredients)
 
 	# 5. Write new table to csv
 	printStep("Step 5: Write new table to CSV")
+	save(new_table, data_file_path)
 
-	print("Finished")
+	printStep("Finished")
 
 def printStep(str):
 	print("\n" + str + "\n")
@@ -33,6 +38,27 @@ def printInfo(str):
 def load(csv_file):
 	printInfo("Loading: " + csv_file)
 	df = pd.read_csv(csv_file)
+	printInfo("Done")
+	return df
+
+def save(df, data_file_path):
+	output_path = data_file_path[:-4] + "-output.csv"
+	printInfo("Saving: " + output_path)
+	df.to_csv(output_path)
+	printInfo("Done")
+
+def findBaseIngredients(df):
+	printInfo("TODO")
+	return {}
+
+def findFoods(df):
+	printInfo("TODO")
+	return {}
+
+def createNewTable(df, foods, base_ingredients):
+	printInfo("TODO")
+	# Iterate through each food and create rows for each ingredient in the food
+	return df
 
 if __name__ == "__main__":
 	main()
