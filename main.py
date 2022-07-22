e7ed910d13b82e84592377c6b397ca7ea464e97f
Tom Hrabchak <tom.hrabchak@qt.io>
2022-07-21 20:27:44 -0400

    first tests

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import os
+import sys
 import pandas as pd
 
-def main():
-	data_file = "fped_1718_trh.csv"
-
+def evaluate_file(data_file):
 	data_folder = os.path.abspath("data")
 	data_file_path = os.path.join(data_folder, data_file)
 
@@ -16,16 +15,15 @@ def main():
 	printStep("Step 3: Find all base ingredients")
 	base_ingredients = findBaseIngredients(df, foods)
 
-	printStep("Test: Pasta Alfredo Test")
-	pasta_alfredo = 58146691
-	recipe = simplifyRecipe(df, pasta_alfredo, base_ingredients)
-	printRecipeSummary(recipe)
+	# printStep("Test: Pasta Alfredo Test")
+	# pasta_alfredo = 58146691
+	# new_table = simplifyRecipe(df, pasta_alfredo, base_ingredients)
 
-	# printStep("Step 4: Create a new table of foods")
-	# new_table = createNewTable(df, foods, base_ingredients)
+	printStep("Step 4: Create a new table of foods")
+	new_table = createNewTable(df, foods, base_ingredients)
 
 	printStep("Step 5: Write new table to CSV")
-	save(recipe, data_file_path)
+	save(new_table, data_file_path)
 
 	printStep("Finished")
 
@@ -75,49 +73,61 @@ def findRecipe(df, food):
 	return df.loc[df['foodcode'] == food]
 
 def simplifyRecipe(df, food, base_ingredients):
-	printInfo("Finding recipe for: " + str(food))
 	recipe = findRecipe(df, food)
-	recipe_weight = recipe['recipeweight'].iloc[0]
+	recipe_weight = recipe['ingredientweightg'].sum()
+	description = str(recipe['mainfooddescription'].iloc[0])
+	printInfo("Simplifying: " + str(food) + " - " + description + " - " + str(recipe_weight))
 	original_base_ingredients = []
 	simplified_ingredients = []
 
 	for index, row in recipe.iterrows():
 		ingredient = row['ingredientcode']
 		ingredient_weight = row['ingredientweightg']
-		ingredient_percentage = (ingredient_weight/recipe_weight)
+		ingredient_description = row['ingredientdescription']
 
 		if (ingredient in base_ingredients):
 			new_row = row.copy()
-			new_row['ingredientpercentage'] = ingredient_percentage
 			original_base_ingredients.append(new_row)
 		else:
-			# This is a complex ingredient, so find the base ingredients
+			# This is a complex ingredient, so find the base ingredients and scale to this recipe
 			complex_ingredient_recipe = simplifyRecipe(df, ingredient, base_ingredients)
-			complex_ingredient_recipe['foodcode'] = food
-			complex_ingredient_recipe['recipeweight'] = recipe_weight
 
-			scaleRecipe(complex_ingredient_recipe, ingredient_weight, ingredient_percentage)
+			printInfo("scaling: " + ingredient_description + " to " + str(ingredient_weight) + " recipe total: " + str(recipe_weight))
+			complex_ingredient_recipe = scaleRecipe(complex_ingredient_recipe, ingredient_weight)
+			printInfo("Scaled recipe:")
+			printRecipeSummary(complex_ingredient_recipe)
 
 			simplified_ingredients.append(complex_ingredient_recipe)
 
-	simplified_ingredients.append(pd.concat(original_base_ingredients, axis=1).T)
+	if (len(original_base_ingredients) > 0):
+		simplified_ingredients.append(pd.concat(original_base_ingredients, axis=1).T)
 	output = pd.concat(simplified_ingredients)
 
+	output['foodcode'] = food
+	output['recipeweight'] = recipe_weight
+	output['ingredientpercentage'] = output['ingredientweightg']/recipe_weight
+
 	printRecipeSummary(output)
+
 	return output
 
-def scaleRecipe(recipe, new_weight, new_overall_scale):
+def scaleRecipe(recipe, new_weight):
+	scaled_recipe = recipe.copy()
 	initial_recipe_weight = recipe['ingredientweightg'].sum()
+	printInfo(initial_recipe_weight)
+
 	for index, row in recipe.iterrows():
-		initial_ingredient_weigth = recipe['ingredientweightg']
-		initial_ingredient_percentage = (initial_ingredient_weigth/initial_recipe_weight)
-		recipe['ingredientweightg'] = (new_weight*initial_ingredient_percentage)
-		recipe['ingredientpercentage'] = (new_overall_scale*initial_ingredient_percentage)
+		initial_ingredient_weight = row['ingredientweightg']
+		initial_ingredient_percentage = (initial_ingredient_weight/initial_recipe_weight)
+		scaled_recipe.loc[index, 'ingredientweightg'] = (new_weight*initial_ingredient_percentage)
+
+	scaled_recipe['recipeweight'] = scaled_recipe['ingredientweightg'].sum()
+	return scaled_recipe
 
 def createNewTable(df, foods, base_ingredients):
 	recipes = []
 	for food in foods:
-		recipes.append(simplifyRecipe(food))
+		recipes.append(simplifyRecipe(df, food, base_ingredients))
 	new_table = pd.concat(recipes)
 
 	printInfo("Number of recipes added: " + str(len(recipes)))
@@ -126,10 +136,16 @@ def createNewTable(df, foods, base_ingredients):
 def printRecipeSummary(recipe):
 	description = recipe['mainfooddescription'].iloc[0]
 	printInfo("Summary: " + str(description) + '\n')
-	print(str(recipe[['ingredientpercentage', 'ingredientweightg', 'ingredientdescription']]) + '\n')
+	print(str(recipe[['ingredientpercentage', 'ingredientweightg', 'ingredientdescription', 'recipeweight']]) + '\n')
 	totals = recipe['ingredientweightg'].sum()
 	print("Total weight: " + str(recipe['ingredientweightg'].sum()))
-	print("total percentage: " + str(recipe['ingredientpercentage'].sum()))
+	print("Total percentage: " + str(recipe['ingredientpercentage'].sum()))
 
 if __name__ == "__main__":
-	main()
+	evaluate_file("fped_0506_trh.csv")
+	evaluate_file("fped_0708_trh.csv")
+	evaluate_file("fped_0910_trh.csv")
+	evaluate_file("fped_1112_trh.csv")
+	evaluate_file("fped_1314_trh.csv")
+	evaluate_file("fped_1516_trh.csv")
+	evaluate_file("fped_1718_trh.csv")
